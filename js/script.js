bc43df6ef195f58a9b288e69f1d3961c13188fda
cvine <rafarakh@gmail.com>
2022-08-19 18:11:39 +0300

    refactor: slider mechanism has been redone

--- js/script.js
+++ js/script.js
@@ -302,35 +302,57 @@ window.addEventListener('DOMContentLoaded', function () {
 //                                  Slider                                    //
 // ************************************************************************** //
 
+let offset = 0;
+let slideIndex = 1;
+
 const slides = document.querySelectorAll('.offer__slide'),
 	prev = document.querySelector('.offer__slider-prev'),
 	next = document.querySelector('.offer__slider-next'),
 	total = document.querySelector('#total'),
-	current = document.querySelector('#current');
- 
-let slideIndex = 1;
+	current = document.querySelector('#current'),
+	slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+	slidesField = document.querySelector('.offer__slider-inner'),
+	width = window.getComputedStyle(slidesWrapper).width;
+
+current.innerText = (slides.length < 10) ? `0${slideIndex}` : slideIndex;
 total.innerText = (slides.length < 10) ? `0${slides.length}` : slides.length;
 
-function showSlide() {
+slidesField.style.width = 100 * slides.length + '%';
+slidesField.style.display = 'flex';
+slidesField.style.transition = '0.5s all';
+
+slidesWrapper.style.overflow = 'hidden';
+
+slides.forEach(slide => {
+	slide.style.width = width;
+});
+
+next.addEventListener('click', () => {
+	if (offset == -width.slice(0, width.length - 2) * (slides.length - 1)) {
+		offset = 0;
+	} else {
+		offset -= +width.slice(0, width.length - 2);
+	}
+	slidesField.style.transform = `translateX(${offset}px)`;
+
+	slideIndex++;
 	if (slideIndex > slides.length)
-	slideIndex = 1;
+		slideIndex = 1;
+
+	current.innerText = (slides.length < 10) ? `0${slideIndex}` : slideIndex;
+})
+
+prev.addEventListener('click', () => {
+	if (offset == 0) {
+		offset = -width.slice(0, width.length - 2) * (slides.length - 1);
+	} else {
+		offset += +width.slice(0, width.length - 2);
+	}
+	slidesField.style.transform = `translateX(${offset}px)`;
+	
+	slideIndex--;
 	if (slideIndex < 1)
 		slideIndex = slides.length;
 
-	slides.forEach(slide => slide.style.display = 'none');
-	slides[slideIndex - 1].style.display = 'block';
-
 	current.innerText = (slides.length < 10) ? `0${slideIndex}` : slideIndex;
-}
-
-showSlide();
-
-prev.addEventListener('click', () => {
-	slideIndex--;
-	showSlide();
-})
-
-next.addEventListener('click', () => {
-	slideIndex++;
-	showSlide();
 })
